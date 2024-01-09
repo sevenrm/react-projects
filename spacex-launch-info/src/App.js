12c8f1100fcb971c5525c3ba503487033ea62b6e
Seven <sevenrm.dev109@gmail.com>
2024-01-09 20:45:55 +0200

    Add the spacex-launch-info

---
+++ spacex-launch-info/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import LaunchInfo from "./components/LaunchInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <LaunchInfo />
+    </div>
+  );
+}
+
+export default App;
